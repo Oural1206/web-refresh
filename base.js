df7cd8e33c15aafdca051458806cd63f801927c5
Oural <71041046+Oural1206@users.noreply.github.com>
2022-04-17 12:22:36 +0200

    Create base.js

---
+++ base.js
@@ -0,0 +1,11 @@
+function refresh() {
+     if (!document.hasFocus()) {
+         location.reload()
+     } else {
+         const remake = setTimeout(refresh, 10)
+     }
+ }
+
+ document.addEventListener('DOMContentLoaded', function(){
+     const start = setTimeout(refresh, 100)
+ });
